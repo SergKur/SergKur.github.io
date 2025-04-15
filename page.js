e7b92dbe1836df0a54246aa5a9b1040cacbfca64
SergKur <dostavka-sm@yandex.ru>
2025-04-15 22:11:47 +0300

    Add files via upload
    JS HomeWorkOne: Создание статической страницы с помощью метода document.write (таблица истинности логических функций).

---
+++ page.js
@@ -0,0 +1,61 @@
+var a, b;
+a = false;
+b = false;
+document.write(
+    '<h1>Таблица истинности логических функций</h1>'
+);
+document.writeln(
+    '<div class="flex_table">'+
+        '<div class="table_title">'+
+        '<div>A</div>'+
+        '<div>B</div>'+
+        '<div>!A</div>'+
+        '<div>A&&B</div>'+
+        '<div>A||B</div>'+
+        '</div>'
+);
+a = false;
+b = false;
+document.writeln(
+         '<div>'+
+         '<div>'+`${a}`+'</div>'+
+         '<div>'+`${b}`+'</div>'+
+         '<div>'+`${!a}`+'</div>'+
+         '<div>'+`${a && b}`+'</div>'+
+         '<div>'+`${a || b}`+'</div>'+
+         '</div>'
+);
+a = false;
+b = true;
+document.writeln(
+         '<div>'+
+         '<div>'+`${a}`+'</div>'+
+         '<div>'+`${b}`+'</div>'+
+         '<div>'+`${!a}`+'</div>'+
+         '<div>'+`${a && b}`+'</div>'+
+         '<div>'+`${a || b}`+'</div>'+
+         '</div>'
+);
+a = true;
+b = false;
+document.writeln(
+         '<div>'+
+         '<div>'+`${a}`+'</div>'+
+         '<div>'+`${b}`+'</div>'+
+         '<div>'+`${!a}`+'</div>'+
+         '<div>'+`${a && b}`+'</div>'+
+         '<div>'+`${a || b}`+'</div>'+
+         '</div>'
+);
+a = true;
+b = true;
+document.writeln(
+         '<div>'+
+         '<div>'+`${a}`+'</div>'+
+         '<div>'+`${b}`+'</div>'+
+         '<div>'+`${!a}`+'</div>'+
+         '<div>'+`${a && b}`+'</div>'+
+         '<div>'+`${a || b}`+'</div>'+
+         '</div>'+
+    '</div>'
+);
