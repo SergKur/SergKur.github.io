cb0e2ba8d40477f9dc033b33baa984e0de6e5c7b
SergKur <dostavka-sm@yandex.ru>
2025-04-16 17:35:12 +0300

    Update page.js
    Избавились от лишних шаблонных кавычек

--- page.js
+++ page.js
@@ -5,57 +5,57 @@ document.write(
     '<h1 class="top">Таблица истинности логических функций</h1>'
 );
 document.writeln(
-    '<div class="flex_table">'+
-        '<div class="table_title">'+
-        '<div>A</div>'+
-        '<div>B</div>'+
-        '<div>!A</div>'+
-        '<div>A&&B</div>'+
-        '<div>A||B</div>'+
-        '</div>'
+    `<div class="flex_table">
+        <div class="table_title">
+        <div>A</div>
+        <div>B</div>
+        <div>!A</div>
+        <div>A&&B</div>
+        <div>A||B</div>
+        </div>`
 );
 a = false;
 b = false;
 document.writeln(
-         '<div>'+
-         '<div>'+`${a}`+'</div>'+
-         '<div>'+`${b}`+'</div>'+
-         '<div>'+`${!a}`+'</div>'+
-         '<div>'+`${a && b}`+'</div>'+
-         '<div>'+`${a || b}`+'</div>'+
-         '</div>'
+        `<div>
+         <div>${a}</div>
+         <div>${b}</div>
+         <div>${!a}</div>
+         <div>${a && b}</div>
+         <div>${a || b}</div>
+         </div>`
 );
 a = false;
 b = true;
 document.writeln(
-         '<div>'+
-         '<div>'+`${a}`+'</div>'+
-         '<div>'+`${b}`+'</div>'+
-         '<div>'+`${!a}`+'</div>'+
-         '<div>'+`${a && b}`+'</div>'+
-         '<div>'+`${a || b}`+'</div>'+
-         '</div>'
+         `<div>
+         <div>${a}</div>
+         <div>${b}</div>
+         <div>${!a}</div>
+         <div>${a && b}</div>
+         <div>${a || b}</div>
+         </div>`
 );
 a = true;
 b = false;
 document.writeln(
-         '<div>'+
-         '<div>'+`${a}`+'</div>'+
-         '<div>'+`${b}`+'</div>'+
-         '<div>'+`${!a}`+'</div>'+
-         '<div>'+`${a && b}`+'</div>'+
-         '<div>'+`${a || b}`+'</div>'+
-         '</div>'
+        `<div>
+         <div>${a}</div>
+         <div>${b}</div>
+         <div>${!a}</div>
+         <div>${a && b}</div>
+         <div>${a || b}</div>
+         </div>`
 );
 a = true;
 b = true;
 document.writeln(
-         '<div>'+
-         '<div>'+`${a}`+'</div>'+
-         '<div>'+`${b}`+'</div>'+
-         '<div>'+`${!a}`+'</div>'+
-         '<div>'+`${a && b}`+'</div>'+
-         '<div>'+`${a || b}`+'</div>'+
-         '</div>'+
-    '</div>'
+        `<div>
+         <div>${a}</div>
+         <div>${b}</div>
+         <div>${!a}</div>
+         <div>${a && b}</div>
+         <div>${a || b}</div>
+         </div>
+    </div>`
 );
